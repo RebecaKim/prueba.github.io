caefc23ecea6a2b87c551f64b189feeea814e5ec
RebecaKim <129077421+RebecaKim@users.noreply.github.com>
2023-04-24 09:27:10 -0500

    main

---
+++ JS/operador.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Convertir Grados</title>
+</head>
+<body>  
+
+    <script>  
+    </script>  
+</body>
+</html>
+
+var celsius1 = prompt("Introduzca la temperatura en grados celsius: ");
+celsius1 =parseFloat(celsius1);
+//Conversión corta:
+//var celsius1 = 
+var Farenheit1 = (9/5)*celsius+32;
+document.write("De celsius a Farenheit es: ", Farenheit1);
